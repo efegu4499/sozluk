317e6c5e8cc341f63bbca4c58989efdf89a85e84
efegu4499 <145839802+efegu4499@users.noreply.github.com>
2023-09-23 13:30:06 +0300

    Create hatalısozluk.py

---
+++ hatalısozluk.py
@@ -0,0 +1,8 @@
+meme_dict = [
+       'conguralitions': 'tebrikler' 
+        ]
+word = input('Anlamadığınız bir kelime yazın(hepsi küçük olacak)')        
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else 
+    print('yanlış kelime')
